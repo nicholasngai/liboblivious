f7190ab8d000bc49b9abef51df750fcfeb4ad2b8
Nicholas Ngai <ngai.nicholas@protonmail.com>
2021-02-11 16:57:51 -0800

    Add o_swap functions

---
+++ include/liboblivious/primitives.h
@@ -0,0 +1,55 @@
+#ifndef __LIBOBLIVIOUS_OSWAP_H
+#define __LIBOBLIVIOUS_OSWAP_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+static inline void o_set8(uint8_t *dest, uint8_t src, bool cond) {
+    uint8_t mask = ~((uint8_t) cond - 1);
+    *dest = (src ^ *dest) & mask;
+}
+
+static inline void o_set16(uint16_t *dest, uint16_t src, bool cond) {
+    uint16_t mask = ~((uint16_t) cond - 1);
+    *dest = (src ^ *dest) & mask;
+}
+
+static inline void o_set32(uint32_t *dest, uint32_t src, bool cond) {
+    uint32_t mask = ~((uint32_t) cond - 1);
+    *dest = (src ^ *dest) & mask;
+}
+
+static inline void o_set64(uint64_t *dest, uint64_t src, bool cond) {
+    uint64_t mask = ~((uint64_t) cond - 1);
+    *dest = (src ^ *dest) & mask;
+}
+
+static inline void o_swap8(uint8_t *a, uint8_t *b, bool cond) {
+    uint8_t mask = ~((uint8_t) cond - 1);
+    *a ^= *b;
+    *b ^= *a & mask;
+    *a ^= *b;
+}
+
+static inline void o_swap16(uint16_t *a, uint16_t *b, bool cond) {
+    uint16_t mask = ~((uint16_t) cond - 1);
+    *a ^= *b;
+    *b ^= *a & mask;
+    *a ^= *b;
+}
+
+static inline void o_swap32(uint32_t *a, uint32_t *b, bool cond) {
+    uint32_t mask = ~((uint32_t) cond - 1);
+    *a ^= *b;
+    *b ^= *a & mask;
+    *a ^= *b;
+}
+
+static inline void o_swap64(uint64_t *a, uint64_t *b, bool cond) {
+    uint64_t mask = ~((uint64_t) cond - 1);
+    *a ^= *b;
+    *b ^= *a & mask;
+    *a ^= *b;
+}
+
+#endif /* liboblivious/o_swap.h */
